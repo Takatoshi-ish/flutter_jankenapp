ea90b82b506024053310f4e030d0412c2c955c3e
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-15 10:00:55 +0900

    デバイスの追加

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
@@ -6,4 +8,7 @@ void main() {
   setUpAll(() async {
     await loadAppFonts();
   });
+  const iPhone55 =
+      Device(name: 'iPhone55', size: Size(414, 736), devicePixelRatio: 3.0);
+  List<Device> devices = [iPhone55];
 }
