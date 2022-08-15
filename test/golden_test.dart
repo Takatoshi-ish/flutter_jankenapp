b3bc48425baf05afcafdc06408fff03d9aa9addf
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-15 10:00:13 +0900

    作成したFontManifestを適応させる

---
+++ test/golden_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+void main() {
+  setUpAll(() async {
+    await loadAppFonts();
+  });
+}
