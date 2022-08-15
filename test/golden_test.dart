825922e18a71d74f9f69127c569e4ed9c4525f70
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-15 10:03:56 +0900

    golden test をriverpod化するための設定を追加

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:flutter_jankenapp/main.dart';
+import 'package:flutter_jankenapp/view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
@@ -11,4 +13,20 @@ void main() {
   const iPhone55 =
       Device(name: 'iPhone55', size: Size(414, 736), devicePixelRatio: 3.0);
   List<Device> devices = [iPhone55];
+
+  testGoldens('normal', (tester) async {
+    ViewModel viewModel = ViewModel();
+
+    await tester.pumpWidgetBuilder(
+      ProviderScope(
+        child: JankenPage(viewModel),
+      ),
+    );
+
+    await multiScreenGolden(
+      tester,
+      'jankenPage_0init',
+      devices: devices,
+    );
+  });
 }
