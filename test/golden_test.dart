e0ee6168ca5634e73821bb251b18c6d75caa47dc
Takatoshi-ish <ishiguro.taka@gmail.com>
2022-08-15 10:14:55 +0900

    golden testで日本語表示する

--- test/golden_test.dart
+++ test/golden_test.dart
@@ -1,14 +1,22 @@
 import 'dart:ui';
 
+import 'package:flutter/services.dart';
 import 'package:flutter_jankenapp/main.dart';
 import 'package:flutter_jankenapp/view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 
+Future<void> loadJapaneseFont() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  final binary = rootBundle.load('assets/NotoSansCJKjp-Regular.ttf');
+  final loader = FontLoader('Roboto')..addFont(binary);
+  await loader.load();
+}
+
 void main() {
   setUpAll(() async {
-    await loadAppFonts();
+    await loadJapaneseFont();
   });
   const iPhone55 =
       Device(name: 'iPhone55', size: Size(414, 736), devicePixelRatio: 3.0);
